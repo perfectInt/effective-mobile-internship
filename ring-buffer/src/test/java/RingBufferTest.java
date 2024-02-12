90a8129a8cc224f2b1f8ed1e11724d8f5dee704d
perfectInt <rs.xdms@gmail.com>
2024-02-12 17:42:41 +0500

    ring buffer

---
+++ ring-buffer/src/test/java/RingBufferTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import ru.effectivemobile.RingBuffer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RingBufferTest {
+    @Test
+    public void testRingBuffer() {
+        RingBuffer<Integer> buffer = new RingBuffer<>(5);
+
+        assertEquals(0, buffer.size());
+
+        buffer.put(1);
+        buffer.put(2);
+        buffer.put(3);
+
+        assertEquals(3, buffer.size());
+
+        assertEquals(1, buffer.poll());
+        assertEquals(2, buffer.poll());
+        assertEquals(3, buffer.poll());
+
+        assertEquals(0, buffer.size());
+    }
+}
