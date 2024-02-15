0dbb4b71200223532ce2d813eec8410deff49f35
perfectInt <rs.xdms@gmail.com>
2024-02-15 13:49:32 +0500

    HW5. Jpa

--- todo-list/src/main/java/ru/effectivemobile/todolist/repository/TaskRepository.java
+++ todo-list/src/main/java/ru/effectivemobile/todolist/repository/TaskRepository.java
@@ -1,44 +1,7 @@
 package ru.effectivemobile.todolist.repository;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-import ru.effectivemobile.todolist.dto.TaskDto;
-import ru.effectivemobile.todolist.handler.ResourceNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.effectivemobile.todolist.entity.Task;
 
-import java.util.List;
-
-@Repository
-@RequiredArgsConstructor
-public class TaskRepository {
-
-    private final JdbcTemplate jdbc;
-
-    public List<TaskDto> getAll() {
-        String query = "SELECT * FROM t_tasks";
-        return jdbc.query(query, new BeanPropertyRowMapper<>(TaskDto.class));
-    }
-
-    public TaskDto findById(Long id) {
-        String query = "SELECT * FROM t_tasks WHERE id = ?";
-        return jdbc.query(query, new BeanPropertyRowMapper<>(TaskDto.class), id)
-                .stream().findFirst().orElseThrow(ResourceNotFoundException::new);
-    }
-
-    public void save(TaskDto task) {
-        String query = "INSERT INTO t_tasks (title, description, completed) VALUES (?, ?, ?)";
-        jdbc.update(query, task.getTitle(), task.getDescription(), task.getCompleted());
-    }
-
-    public void update(TaskDto task) {
-        String query = "UPDATE t_tasks SET title = ?, description = ?, completed = ? WHERE id = ?";
-        jdbc.update(query, task.getTitle(), task.getDescription(), task.getCompleted(), task.getId());
-    }
-
-    public void delete(Long id) {
-        String query = "DELETE FROM t_tasks WHERE id = ?";
-        jdbc.update(query, id);
-    }
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
--- todo-list/src/main/java/ru/effectivemobile/todolist/service/TaskService.java
+++ todo-list/src/main/java/ru/effectivemobile/todolist/service/TaskService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.effectivemobile.todolist.dto.Response;
 import ru.effectivemobile.todolist.dto.TaskDto;
+import ru.effectivemobile.todolist.entity.Task;
+import ru.effectivemobile.todolist.handler.ResourceNotFoundException;
+import ru.effectivemobile.todolist.mapper.TaskMapper;
 import ru.effectivemobile.todolist.repository.TaskRepository;
 
 import java.util.List;
@@ -17,32 +20,36 @@ import java.util.List;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
     @Cacheable("tasks")
     public Response<List<TaskDto>> getAllTasks() {
-        return new Response<>(true, taskRepository.getAll());
+        List<Task> tasks = taskRepository.findAll();
+        List<TaskDto> taskDtos = tasks.stream().map(taskMapper::mapFromEntity).toList();
+        return new Response<>(true, taskDtos);
     }
 
     @Cacheable("tasks")
     public Response<TaskDto> getTaskById(Long id) {
-        return new Response<>(true, taskRepository.findById(id));
+        TaskDto taskDto = taskMapper.mapFromEntity(taskRepository.findById(id).orElseThrow(ResourceNotFoundException::new));
+        return new Response<>(true, taskDto);
     }
 
     @Transactional
     @CachePut(value = "tasks", key = "#taskDto.title")
     public void createTask(TaskDto taskDto) {
-        taskRepository.save(taskDto);
+        taskRepository.save(taskMapper.mapFromDto(taskDto));
     }
 
     @Transactional
     @CachePut(value = "tasks", key = "#taskDto.title")
     public void updateTask(TaskDto taskDto) {
-        taskRepository.update(taskDto);
+        taskRepository.save(taskMapper.mapFromDto(taskDto));
     }
 
     @Transactional
     @CacheEvict("tasks")
     public void deleteTask(Long id) {
-        taskRepository.delete(id);
+        taskRepository.deleteById(id);
     }
 }
