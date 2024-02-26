3bbea1d77e7d6534f210afa08cddcea6d84ba3b3
perfectInt <rs.xdms@gmail.com>
2024-02-26 19:51:01 +0500

    HW7. Kafka Log4j2 Appender

---
+++ kafka-log4j2-appender/src/main/java/ru/effectivemobile/KafkaProducerFactory.java
@@ -0,0 +1,19 @@
+package ru.effectivemobile;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaProducerFactory {
+
+    public static Producer<String, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return new KafkaProducer<>(props);
+    }
+}
